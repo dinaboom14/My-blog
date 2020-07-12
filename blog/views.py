804a1cc0ca9bc83c3ba720596401e58b9d47b365
Alexandr <dinaboom.02@gmail.com>
2020-07-13 03:13:29 +0600

    My app

---
+++ blog/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .models import Post, Comment
+from .forms import CommentForm
+from taggit.models import Tag
+from django.db.models import Count
+
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)  # 3 поста на каждой странице
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом, поставим первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если страница больше максимальной, доставить последнюю страницу результатов
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'page': page,
+                   'posts': posts,
+                   'tag': tag})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    # Список активных комментариев к этой записи
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    similar_posts = ""
+    if request.method == 'POST':
+        # Комментарий был опубликован
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Создайте объект Comment, но пока не сохраняйте в базу данных
+            new_comment = comment_form.save(commit=False)
+            # Назначить текущий пост комментарию
+            new_comment.post = post
+            # Сохранить комментарий в базе данных
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+        # Список похожих постов
+        post_tags_ids = post.tags.values_list('id', flat=True)
+        similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+            .exclude(id=post.id)
+        similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+            .order_by('-same_tags', '-publish')[:4]
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
+
+
